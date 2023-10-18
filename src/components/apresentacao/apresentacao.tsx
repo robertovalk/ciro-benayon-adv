0aaf50aba2497e95e13f381ca58ff3a972ce4db7
robertovalk <86753780+robertovalk@users.noreply.github.com>
2023-10-18 14:11:08 -0400

    Update apresentacao.tsx

--- src/components/apresentacao/apresentacao.tsx
+++ src/components/apresentacao/apresentacao.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import Image from "next/image"
-import styled from "styled-components"
+import Image from "next/image";
+import styled from "styled-components";
 
 
 const Container = styled('div') `
